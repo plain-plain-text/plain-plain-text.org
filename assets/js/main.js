84d2891ea17abc38516b2f03fec84a5e6dcb2411
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-04-29 14:45:47 -0400

    Get something resembling a site up.

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Add icon for external links & flesh out ToC
+$(document).ready(function() {
+  let externalLink = $.parseHTML("<span>&nbsp;<i style='vertical-align: baseline; font-size: 60%;' class='fa fa-small fa-external-link-alt'></i></span>");
+  $("a[href^='http']:not(a:has(img))").append(externalLink);
+  $("a[href^='http']").attr("target", "_blank");
+
+  // flesh out table of contents.
+  $("#tocList").append(function(){
+    let contents =  ""
+    $("article > h2").each(function(){
+      contents += "<div class='nav-item'><a class='nav-link' href='#" + $( this ).attr("id") + "'>" + $( this ).text() + "</a></div>\n";
+    });
+    if(contents.length > 0){
+      $("#tocDiv").attr("style", "display: block;");
+    }
+    return contents;
+  });
+}); 
+
